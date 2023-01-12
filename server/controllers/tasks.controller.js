2e1924f9e09757efd446ec104aeee732eaa33935
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-11 18:27:04 -0600

    Get tasks endpoint

--- server/controllers/tasks.controller.js
+++ server/controllers/tasks.controller.js
@@ -1,11 +1,22 @@
 import { pool } from "../db.js";
 
-export const getTasks = (req, res) => {
-  res.send("Obteniendo tareas");
+export const getTasks = async (req, res) => {
+  const [result] = await pool.query(
+    "SELECT * FROM tasks ORDER BY createAt ASC"
+  );
+  res.json(result);
 };
 
-export const getTask = (req, res) => {
-  res.send("Obteniendo una tarea");
+export const getTask = async (req, res) => {
+  const { id } = req.params;
+  const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [id]);
+
+  if (result.length == 0)
+    return res.status(404).json({
+      message: "Tasks not found",
+    });
+
+  res.json(result[0]);
 };
 
 export const createTask = async (req, res) => {
