0eb9cf8686c24dc83ba71f9692676ceb2bcb2653
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-11 20:51:13 -0600

    Frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import TasksPage from './pages/TasksPage'
+import TaskForm from './pages/TaskForm'
+import NotFound from './pages/NotFound'
+
 function App() {
   return (
-    <div>Hello world</div>
+    <Routes>
+      <Route path='/' element={<TasksPage />} />
+      <Route path='/new' element={<TaskForm />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
