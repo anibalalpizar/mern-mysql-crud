3c9cb241e6337308f95be7596ddefb6092abe568
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-11 18:04:46 -0600

    Create tasks endpoint

--- server/controllers/tasks.controller.js
+++ server/controllers/tasks.controller.js
@@ -1,3 +1,5 @@
+import { pool } from "../db.js";
+
 export const getTasks = (req, res) => {
   res.send("Obteniendo tareas");
 };
@@ -6,8 +8,17 @@ export const getTask = (req, res) => {
   res.send("Obteniendo una tarea");
 };
 
-export const createTask = (req, res) => {
-  res.send("Creando tareas");
+export const createTask = async (req, res) => {
+  const { title, description } = req.body;
+  const [result] = await pool.query(
+    "INSERT INTO tasks(title, description) VALUES (?,?)",
+    [title, description]
+  );
+  res.json({
+    id: result.insertId,
+    title,
+    description,
+  });
 };
 
 export const updateTask = (req, res) => {
